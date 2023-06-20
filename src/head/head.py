85e2ff41ce866653a228d01dff5f35f906064a63
Flora <flomaidar@gmail.com>
2023-06-20 18:31:48 -0300

    working head interface with moveit zed_controller

--- src/head/head.py
+++ src/head/head.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python3
 
 from __future__ import division
+import sys
 import rospy
-from std_msgs.msg import Float32MultiArray, Float32
+import moveit_commander
+from std_msgs.msg import Float32MultiArray, Float32, Float64
 from hera_control.srv import Head_service
-from dynamixel_workbench_msgs.srv import DynamixelCommand
 
 
 class Head:
     def __init__(self):
         self.goal = Float32()
         rospy.Service('head_interface', Head_service, self.handler)
-        self.head = rospy.ServiceProxy('/dynamixel_controller/dynamixel_command', DynamixelCommand)
-        rospy.loginfo('Head ready!')
-        self.reset()
-        
-    def reset(self):
-        success = self.head('', 9, 'Goal_Position', 2048)
+        moveit_commander.roscpp_initialize(sys.argv)
+        # self.pub = rospy.Publisher("/zed_controller/command", Float64, queue_size=10)
+        self.head = moveit_commander.MoveGroupCommander('zed')
+        self.head.set_max_acceleration_scaling_factor(1.0)
+        self.head.set_max_velocity_scaling_factor(1.0)
+
+    def handler(self, request,):
+        function_name = request.type.lower()
+        pose = request.goal
+
+        functions = {
+            'up': lambda pose=None: self.execute_pose('up'),
+            'down': lambda pose=None: self.execute_pose('down'),
+        }
+
+        try:
+            result = functions[function_name](pose)
+            return str(result)
+
+        except KeyError:
+            rospy.logerr('Invalid function name %s' % function_name)
+            return "Invalid function name: {}".format(function_name)
+    
+    def execute_pose(self, pose_name):
+        head = self.head
+        head.set_named_target(pose_name)
+        success = head.go(wait=True)
+        head.stop()
         return success
-
-    def handler(self, request):
-        type = request.type.lower()
-        goal = request.goal
-
-        goal.z = 3.14 if goal.z >= 3.14 else goal.z
-        goal.z = 2.0 if goal.z <= 2.0 else goal.z
-
-        goal.z = (2048 * goal.z) / 3.14
-
-        if type == 'reset':
-            success = self.reset()
-        else:
-            success = self.head('', 9, 'Goal_Position', int(goal.z))
-
-        if success:
-            return "succeeded"
-        else:
-            return "failed"
 
 
 if __name__ == '__main__':
