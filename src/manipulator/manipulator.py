87c3a3764cee941c5d75b06f6a315b20a2c7d96b
khaled hazime <hazimekhaled01@gmail.com>
2024-05-16 14:40:45 -0300

    pick bottom pose

--- src/manipulator/manipulator.py
+++ src/manipulator/manipulator.py
@@ -102,6 +102,7 @@ class Manipulator:
             'serving_cereal_left': lambda pose=None: self.serving_cereal('left'),
             'serving_left': lambda pose=None: self.serving('left'),
             'pick': lambda pose: self.pick(pose),
+            'pick_bottom': lambda pose: self.pick_bottom(pose),
             'place_with_pose': lambda pose: self.place_with_pose(pose),
             'close_with_box': lambda pose=None: self.close_with_box(),
             'place': lambda pose=None: self.place('place'),
@@ -331,14 +332,45 @@ class Manipulator:
         self.clear_octomap()
 
 
-        self.addCylinder(self.box_name, 0.1, 0.0125, (self.coordinates.x), (self.coordinates.y + 0.06), self.coordinates.z)
+        self.addCylinder(self.box_name, 0.1, 0.0125, (self.coordinates.x), (self.coordinates.y + 0.03), self.coordinates.z)
         pose.position.x -= 0.12
-        pose.position.y += 0.06
+        pose.position.y += 0.03
         # rospy.sleep(2)
 
         target_pose = copy.deepcopy(pose)
         self.arm.set_pose_target(target_pose)
         self.execute_pose(self.head, 'up')
+        rospy.sleep(1)
+        success = self.arm.go(wait=True)
+        if success:
+            # tirei o clear antes do attach, pois estava demorando muito
+            # self.clear_octomap()
+            # rospy.sleep(1)
+            self.attach_box()
+            self.clear_octomap()
+            success2 = self.execute_pose(self.hand,'hard_close')
+            # self.execute_pose(self.arm,'attack')
+            return success2
+        else:
+            self.detach_box()
+            self.remove_box()
+        return success
+    
+    def pick_bottom(self,pose):
+        self.execute_pose(self.hand,'open')
+        self.execute_pose(self.head, 'way_down')
+        rospy.sleep(2)
+        self.clear_octomap()
+
+
+        self.addCylinder(self.box_name, 0.1, 0.0125, (self.coordinates.x), (self.coordinates.y + 0.03), self.coordinates.z)
+        pose.position.x -= 0.12
+        pose.position.y += 0.03
+        # rospy.sleep(2)
+
+        target_pose = copy.deepcopy(pose)
+        self.arm.set_pose_target(target_pose)
+        #self.execute_pose(self.head, 'up')
         rospy.sleep(1)
         success = self.arm.go(wait=True)
         if success:
