7d16324005b030ec873f5ed03673b8aaec37cdef
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2024-06-17 18:40:45 -0300

    removing manipulator uses in point pixel

--- src/manipulator/manipulator.py
+++ src/manipulator/manipulator.py
@@ -418,16 +418,16 @@ class Manipulator:
         cam_x_pixel = 1280
 
         pixel_rad = ((-1.4/cam_x_pixel)*pixel) + 0.825
-        manip_point_dist = law_cosines(manip_cam_dist, pixel_rad, point_dist)
-        manip_point_rad = law_sines(point_dist, pixel_rad, manip_point_dist)
-        print(manip_point_rad)
-        manip_rad = sine_to_rad(manip_point_rad)
-
-        manip_rad = manip_rad if pixel_rad > 0 else -manip_rad
-
-        # self.execute_pose(self.hand, 'w')
-        self.execute_pose(self.arm, 'point')
-        self.move_joint(1, manip_rad)
+        # manip_point_dist = law_cosines(manip_cam_dist, pixel_rad, point_dist)
+        # manip_point_rad = law_sines(point_dist, pixel_rad, manip_point_dist)
+        # print(manip_point_rad)
+        # manip_rad = sine_to_rad(manip_point_rad)
+        #
+        # manip_rad = manip_rad if pixel_rad > 0 else -manip_rad
+        #
+        # # self.execute_pose(self.hand, 'w')
+        # self.execute_pose(self.arm, 'point')
+        self.move_joint(10, pixel_rad)
         return True
 
     def point_rad(self, angle):
